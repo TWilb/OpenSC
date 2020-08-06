3139d3a7333e8c70ef9c838ba9ea842025302c14
Tim Wilbrink <tim@picoreo.com>
2020-08-06 11:38:36 +0200

    Cleaning up the mixed up key / binary permissions

--- .github/add_signing_key.sh
+++ .github/add_signing_key.sh
@@ -20,9 +20,9 @@ security set-keychain-settings -t 3600 -u $KEY_CHAIN
 curl -L https://developer.apple.com/certificationauthority/AppleWWDRCA.cer > AppleWWDRCA.cer
 security import AppleWWDRCA.cer  -k  ~/Library/Keychains/$KEY_CHAIN -T /usr/bin/codesign
 security import certificate_installer.cer  -k  ~/Library/Keychains/$KEY_CHAIN -T /usr/bin/codesign
-security import certificate_installer.p12  -k  ~/Library/Keychains/$KEY_CHAIN -P $KEY_PASSWORD -T /usr/bin/codesign
+security import certificate_installer.p12  -k  ~/Library/Keychains/$KEY_CHAIN -P $KEY_PASSWORD -T /usr/bin/productsign
 security import certificate_application.cer  -k  ~/Library/Keychains/$KEY_CHAIN -T /usr/bin/codesign
-security import certificate_application.p12  -k  ~/Library/Keychains/$KEY_CHAIN -P $KEY_PASSWORD -T /usr/bin/productsign
+security import certificate_application.p12  -k  ~/Library/Keychains/$KEY_CHAIN -P $KEY_PASSWORD -T /usr/bin/codesign
 security unlock-keychain -p travis $KEY_CHAIN
 
 # https://docs.travis-ci.com/user/common-build-problems/#mac-macos-sierra-1012-code-signing-errors
